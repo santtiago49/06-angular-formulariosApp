fda5d8adf651cd68cc1a549dbf3c3ce1e2b3aa12
Santiago <santtiago49@gmail.com>
2022-02-07 11:08:49 -0300

    Directivas personalizadas - customMin - opcional

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -17,17 +17,18 @@ export class BasicosComponent implements OnInit {
   }
 
   guardar(){
-    console.log(this.miFormulario);
+    // console.log(this.miFormulario);
+    this.miFormulario.resetForm()
   }
 
   nombreValido():boolean{
-    return this.miFormulario?.controls.producto?.value < 0 &&
+    return this.miFormulario?.controls.producto?.invalid &&
            this.miFormulario?.controls.producto?.touched
   }
-
+  
   precioValido():boolean{
-    return this.miFormulario?.controls.precio?.invalid &&
-           this.miFormulario?.controls.precio?.touched
+    return this.miFormulario?.controls.precio?.touched &&
+           this.miFormulario?.controls.precio?.invalid
   }
 
 }
